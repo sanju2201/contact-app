08c72cd7d7872d630f78551173566c095dc4e4dd
SanjuManna <sanjumanna2201@gmail.com>
2023-02-09 00:42:28 +0530

    Commit

--- src/componants/App.js
+++ src/componants/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+// import ContactCard from "./ContactCard";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<div className="ui container">
+    <Header />
+    <AddContact />
+    <ContactList />
+  </div>);
 }
 
 export default App;
